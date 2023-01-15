89c13c5a42f6e677e3d884ef082d0ecd68bca8d6
Michael G. Noll <miguno@miguno.com>
2023-01-15 21:25:47 +0100

    Add semicolon

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use rust_template::sum;
 
 pub fn main() {
     println!("Hello, world!");
-    println!("2 plus 2 ist {}, immer und überall!", sum(2, 2))
+    println!("2 plus 2 ist {}, immer und überall!", sum(2, 2));
 }
