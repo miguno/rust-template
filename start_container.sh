d2ea41ac23acee3cc5cdbb826268a1876de26b5b
Michael G. Noll <miguno@miguno.com>
2023-01-18 22:38:05 +0100

    Remove ports (leftover copy-paste)

--- start_container.sh
+++ start_container.sh
@@ -7,8 +7,6 @@ set -uo pipefail
 
 declare -r IMAGE_NAME="miguno/rust-template"
 declare -r IMAGE_TAG="latest"
-declare -r APP_PORT="8123"
 
-echo "Starting container for image '$IMAGE_NAME:$IMAGE_TAG', exposing port $APP_PORT/tcp"
-docker run -p "$APP_PORT":"$APP_PORT" "$IMAGE_NAME":"$IMAGE_TAG"
-
+echo "Starting container for image '$IMAGE_NAME:$IMAGE_TAG'"
+docker run "$IMAGE_NAME":"$IMAGE_TAG"
