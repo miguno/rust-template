7a182bafeff182f03f065b1a5d1a5dec95f1f808
Michael G. Noll <miguno@miguno.com>
2023-06-03 03:48:53 +0200

    Add doc strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+/// Computes the sum of two numbers.
 pub fn sum(a: i32, b: i32) -> i32 {
     a + b
 }
 
+/// Doubles the numbers in the vector.
 pub fn doubler(v: Vec<i32>) -> Vec<i32> {
     v.into_iter().map(|x| x * 2).collect()
 }
