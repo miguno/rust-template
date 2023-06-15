befc2f08d8ad23aa644812e8966acf17b9d02d1d
Michael G. Noll <miguno@miguno.com>
2023-06-15 21:44:10 +0200

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 /// Computes the sum of two numbers.
+///
+/// For demonstration purposes only. Intentionally does not check for integer
+/// overflow/underflow.
 pub fn sum(a: i32, b: i32) -> i32 {
     a + b
 }
 
 /// Doubles the numbers in the vector.
+///
+/// For demonstration purposes only. Intentionally does not check for integer
+/// overflow/underflow.
 pub fn doubler(v: Vec<i32>) -> Vec<i32> {
     v.into_iter().map(|x| x * 2).collect()
 }
